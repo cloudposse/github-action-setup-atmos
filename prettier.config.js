b064839860e6c6824d6de39f1459d02e67624462
Matt Calhoun <matt@mattcalhoun.com>
2024-03-06 13:58:21 -0500

    update to node 20

--- prettier.config.js
+++ prettier.config.js
@@ -1,9 +1,10 @@
 /** @type {import("prettier").Config} */
 const config = {
+  printWidth: 120,
   singleQuote: false,
   tabWidth: 2,
-  trailingComma: 'none',
-  useTabs: false,
+  trailingComma: "none",
+  useTabs: false
 };
 
 module.exports = config;
