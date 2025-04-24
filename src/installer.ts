ec9971355091958b5518ace2482556e058f9bb16
Benbentwo <Benbentwo@users.noreply.github.com>
2025-04-24 17:26:01 +0000

    chore: [skip-ci] format code and generate build artifacts

--- src/installer.ts
+++ src/installer.ts
@@ -20,11 +20,8 @@ import * as sys from "./system";
 // 1.8.5beta1 => 1.8.5-beta.1, 1.8.5rc1 => 1.8.5-rc.1
 // 1.172.0-rc.1 => 1.172.0-rc.1
 export const makeSemver = (version: string): string => {
-  version = version.replace("beta", "-beta.")
-    .replace("rc", "-rc.")
-    .replace("--", "-")
-    .replace("..", ".");
-  
+  version = version.replace("beta", "-beta.").replace("rc", "-rc.").replace("--", "-").replace("..", ".");
+
   const parts = version.split("-");
 
   const semVersion = semver.coerce(parts[0])?.version;
